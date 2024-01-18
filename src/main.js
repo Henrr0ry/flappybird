fcd4543c92986540cf4bfb47aeb39aad7a7d01e7
Henrr0ry <112340089+Henrr0ry@users.noreply.github.com>
2024-01-18 10:20:51 +0100

    update

---
+++ src/main.js
@@ -0,0 +1,7 @@
+let game = document.getElementById("game");
+var canv = game.getContext("2d");
+
+canv.font = "30px Arial";
+canv.fillText("Hello World", 10, 50);
+
+//context.drawImage(obrazek, x, y, vyska, sirka);
